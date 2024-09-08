999aaf6dff686fb6f8f73f6b035ed7330b3eab0b
tonynguyen <tonyjsfreelancer@gmail.com>
2024-09-08 11:40:04 +0700

    pure css

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+const issuesList = document.getElementById('issuesList');
+
+console.log('issuesList: ', issuesList)
+
+let issues = [
+  {
+    id: 1,
+    title: 'Noteworthy technology acquisitions 2021',
+    description: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+    author: 'tony',
+    severity: 'low',
+    status: 'new'
+  }
+];
+
+// TODO: render UI list todos
